23baf65efef4bc2892ee0576a380b055dfa98935
Erwin GO <erwingaitano@gmail.com>
2017-07-08 18:18:33 -0500

    adding pug/sass and small build to create the dist files

---
+++ build.sh
@@ -0,0 +1,13 @@
+mkdir dist
+
+echo 'generating index.html'
+pug -P src/index.pug -o .
+
+echo '\ngenerating dist/_css/styles.css'
+mkdir dist/_css
+node-sass src/_css/styles.scss > dist/_css/styles.css
+echo 'done'
+
+echo '\ngenerating dist/_fonts'
+mkdir dist/_fonts
+icon-font-generator src/_fonts/*.svg -o dist/_fonts --types 'woff, woff2'
