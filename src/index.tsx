481d9d6bcf391242c439ff82e52fb375cca53326
Erwin GO <erwingaitano@gmail.com>
2020-05-01 04:19:24 -0500

    changed logo and brand color

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,63 @@
-import * as React from "react";
-import * as ReactDom from "react-dom";
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
 
-import "./_css/index.scss";
-import "./_fonts/icons/font";
+import './_css/index.scss';
+import './_fonts/icons/font';
 
 const items = [
-  {
-    name: "Github",
-    icon: "is-github",
-    target: "blank",
-    href: "https://github.com/goerwin"
-  },
-  {
-    name: "LinkedIn",
-    icon: "is-linkedin",
-    target: "blank",
-    href: "https://www.linkedin.com/in/erwin-gait%C3%A1n-ospino-61438453/"
-  },
-  {
-    name: "Email",
-    icon: "is-email-button",
-    href: "mailto:erwingaitano@gmail.com"
-  }
+    {
+        name: 'Github',
+        icon: 'is-github',
+        target: '_blank',
+        href: 'https://github.com/goerwin',
+    },
+    {
+        name: 'LinkedIn',
+        icon: 'is-linkedin',
+        target: '_blank',
+        href: 'https://www.linkedin.com/in/erwin-gait%C3%A1n-ospino-61438453/',
+    },
+    {
+        name: 'Email',
+        icon: 'is-email-button',
+        href: 'mailto:erwingaitano@gmail.com',
+    },
+    {
+        name: 'Blog',
+        icon: 'is-blog',
+        target: '_blank',
+        href: 'https://goerwin.co/blog',
+    },
 ];
 
 ReactDom.render(
-  <div className="App">
-    <p className="App__title">
-      {"{"}
-      <span className="App__title__hl">go</span>erwin{"}"}
-    </p>
-    <div className="App__socialitems">
-      {items.map(({ href, icon, name, target }) => (
-        <a
-          key={name}
-          className="App__socialitems__item"
-          onClick={() => (window as any).ga("send", "event", "link", "click", name)}
-          title={name}
-          target={target}
-          href={href}
-        >
-          <span className={`icon ${icon}`} />
-        </a>
-      ))}
-    </div>
-  </div>,
-  document.getElementById("app")
+    <div className="App">
+        <p className="App__title">
+            {'{'}
+            <span className="App__title__hl">go</span>erwin{'}'}
+        </p>
+        <div className="App__socialitems">
+            {items.map(({ href, icon, name, target }) => (
+                <a
+                    key={name}
+                    className="App__socialitems__item"
+                    onClick={() =>
+                        (window as any).ga(
+                            'send',
+                            'event',
+                            'link',
+                            'click',
+                            name
+                        )
+                    }
+                    title={name}
+                    target={target}
+                    href={href}
+                >
+                    <span className={`icon ${icon}`} />
+                </a>
+            ))}
+        </div>
+    </div>,
+    document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,110 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const googleHelpers = require('helpers/dist/universal/google');
 
 const getAbsPath = (pathStr) => path.resolve(__dirname, pathStr);
 
-const distFolder = getAbsPath("dist");
-const isProd = process.env.NODE_ENV === "production";
+const distFolder = getAbsPath('dist');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  mode: isProd ? "production" : "development",
-  entry: {
-    index: getAbsPath("src/index.tsx"),
-  },
-  devServer: {
-    contentBase: distFolder,
-  },
-  output: {
-    path: distFolder,
-    filename: isProd ? "[name].bundle.[contenthash].js" : "[name].bundle.js",
+    mode: isProd ? 'production' : 'development',
+    entry: {
+        index: getAbsPath('src/index.tsx'),
+    },
+    devServer: {
+        contentBase: distFolder,
+    },
+    output: {
+        path: distFolder,
+        filename: isProd
+            ? '[name].bundle.[contenthash].js'
+            : '[name].bundle.js',
 
-    // Prefix path for all the relative-url assets required inside the source code.
-    // Eg. if you have a .css file that has background-image: url('a.png') <<Nno absolute path>>
-    // and your public path is set to http://cdn.example.com, the bundled
-    // file will have background-image: url(http://cdn.example.com/a.png).
+        // Prefix path for all the relative-url assets required inside the source code.
+        // Eg. if you have a .css file that has background-image: url('a.png') <<Nno absolute path>>
+        // and your public path is set to http://cdn.example.com, the bundled
+        // file will have background-image: url(http://cdn.example.com/a.png).
 
-    // TODO: webpack default is '' but i have to explicitely set it to './'
-    // because of a webfonts-loader issue
-    // https://github.com/jeerbl/webfonts-loader/issues/28
-    // publicPath: './'
-  },
+        // TODO: webpack default is '' but i have to explicitely set it to './'
+        // because of a webfonts-loader issue
+        // https://github.com/jeerbl/webfonts-loader/issues/28
+        // publicPath: './'
+    },
 
-  devtool: "source-map",
+    devtool: 'source-map',
 
-  module: {
-    rules: [
-      // TYPESCRIPT
-      { test: /\.tsx?$/, include: getAbsPath("src"), use: ["ts-loader"] },
+    module: {
+        rules: [
+            // TYPESCRIPT
+            { test: /\.tsx?$/, include: getAbsPath('src'), use: ['ts-loader'] },
 
-      // SCSS
-      {
-        test: /\.s?css$/,
-        include: getAbsPath("src"),
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-
-      // IMAGES
-      {
-        test: /\.(png|jpg|svg)$/,
-        include: getAbsPath("src"),
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[contenthash].[ext]",
+            // SCSS
+            {
+                test: /\.s?css$/,
+                include: getAbsPath('src'),
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
-          },
-        ],
-      },
 
-      // SVG ICONS
-      {
-        test: /[\\\/]_fonts[\\\/].*[\\\/]font\.js$/,
-        include: getAbsPath("src"),
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          "css-loader",
-          "webfonts-loader",
+            // IMAGES
+            {
+                test: /\.(png|jpg|svg)$/,
+                include: getAbsPath('src'),
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[contenthash].[ext]',
+                        },
+                    },
+                ],
+            },
+
+            // SVG ICONS
+            {
+                test: /[\\\/]_fonts[\\\/].*[\\\/]font\.js$/,
+                include: getAbsPath('src'),
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader',
+                    'webfonts-loader',
+                ],
+            },
         ],
-      },
+    },
+
+    plugins: [
+        // contentash is the file's checksum, useful for caching purposes
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: 'body',
+            template: getAbsPath('src/index.html'),
+            filename: 'index.html',
+            templateParameters: {
+                title: 'GO Erwin',
+                googleAnalyticsScript: googleHelpers.getAnalyticsScript(
+                    'UA-102237991-1'
+                ),
+                googleAdSenseScript: googleHelpers.getAdSenseScript(
+                    'ca-pub-3590698513623239'
+                ),
+            },
+        }),
     ],
-  },
 
-  plugins: [
-    // contentash is the file's checksum, useful for caching purposes
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      inject: "body",
-      template: getAbsPath("src/index.html"),
-      filename: "index.html",
-    }),
-  ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
 
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-
-    // Where to look when using things like "import 'lodash';"
-    modules: [getAbsPath("node_modules")],
-  },
+        // Where to look when using things like "import 'lodash';"
+        modules: [getAbsPath('node_modules')],
+    },
 };
